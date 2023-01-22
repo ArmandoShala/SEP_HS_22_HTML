515248ce3fe06ea4fdf63dd7a0b0b68b8882842d
Armando <dreiteilig.backoefen_0f@icloud.com>
2023-01-22 14:25:07 +0100

    Initial commit

---
+++ app/src/test/java/ch/zhaw/shalaar3/sep_hs_22_html/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package ch.zhaw.shalaar3.sep_hs_22_html
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
