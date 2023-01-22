f3bb9458d98f18b66a1e3006fbb4745f1c238fb8
Armando <dreiteilig.backoefen_0f@icloud.com>
2023-01-22 18:23:48 +0100

    Layout Done in Kotlin

--- app/src/main/java/ch/zhaw/shalaar3/sep_hs_22_html/MainActivity.kt
+++ app/src/main/java/ch/zhaw/shalaar3/sep_hs_22_html/MainActivity.kt
@@ -2,15 +2,11 @@ package ch.zhaw.shalaar3.sep_hs_22_html
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        val webView = findViewById(R.id.webview) as WebView;
-        webView.loadUrl("file:///android_asset/index.html");
 
     }
 }
